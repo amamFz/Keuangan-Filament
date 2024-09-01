e11fef392bc43b1a7f5b587327ee48406685637f
Ilham Fauzi <if7232232@gmail.com>
2024-09-01 17:40:42 +0700

    first commit

---
+++ app/Filament/Widgets/WidgetExpenseChart.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Illuminate\Support\Carbon;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\Transaction;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+
+class WidgetExpenseChart extends ChartWidget
+{
+    use InteractsWithPageFilters;
+
+    protected static ?string $heading = 'Pengeluaran';
+    protected static string $color = 'danger';
+
+    protected function getData(): array
+    {
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null; // Nilai default
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now()->endOfMonth();
+
+        $data = Trend::query(Transaction::expense())
+            ->between(
+                start: $startDate,
+                end: $endDate,
+            )
+            ->perDay()
+            ->sum('amount');
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Pengeluaran per hari',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
+        ];
+    }
+
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
